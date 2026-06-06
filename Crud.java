e81972f41f6464099ffd8a53c5a35bd6d245f007
Pignamssi-Kibandou-Gloria <kibandou32@gmail.com>
2026-06-06 10:55:11 +0000

    Ajout de la fonctionalité Supprimer un produit

--- Crud.java
+++ Crud.java
@@ -11,7 +11,14 @@ public class Crud {
         quantite = pQuantite;
         prix = pPrix;
     }
-
+    public void supprimerProduit(){
+        for(int i=0; i<nomProduit.length();i++){
+            while (this.nomProduit.equals(nomProduit)) {
+                    this.nomProduit.remove(i);
+                    System.out.println("Le produit " + nomProduit + " a été supprimé.");
+                }
+        }
+    }
     // 2- Modifier : une méthode ModifierProduit(String pNomProduit,int
     // pQuantite,double pPrix) qui ne retourne rien (void)
 
