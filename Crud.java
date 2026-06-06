e5fdb6ea8ab6fa0633ebc2bc28637af3cb92496e
Jean Urbain Koku Dziedzom KUMAKO <jeanur191207@gmail.com>
2026-06-06 11:34:17 +0000

    fonctionnalité modifier stock

--- Crud.java
+++ Crud.java
@@ -15,38 +15,18 @@ public class Crud {
     // 2- Modifier : une méthode ModifierProduit(String pNomProduit,int
     // pQuantite,double pPrix) qui ne retourne rien (void)
 
-// Getters
-public String getNomProduit() {
-    return nomProduit;
-}
 
-public int getQuantite() {
-    return quantite;
-}
-
-public double getPrix() {
-    return prix;
-}
 
 // Setters
-public void setNomProduit(String nomProduit) {
+public void ModifierProduit(String nomProduit,double prix,int quantite) {
     this.nomProduit = nomProduit;
-}
-
-public void setQuantite(int quantite) {
     this.quantite = quantite;
-}
-
-public void setPrix(double prix) {
     this.prix = prix;
 }
 
-// Modifier
-public void modifierProduit(String pNomProduit, int pQuantite, double pPrix) {
-    setNomProduit(pNomProduit);
-    setQuantite(pQuantite);
-    setPrix(pPrix);
+
 }
+
 
     
 
